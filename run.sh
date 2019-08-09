7e48456ae8817fbd269e589e0568e600130d9cc8
linyingjie <robot@ascs.tech>
2019-08-09 10:34:42 +0800

    bugfixed:pip3命令找不到。修改为pip

--- run.sh
+++ run.sh
@@ -25,8 +25,8 @@ cat << EOF > ${download_dir}/download-python-packages.sh
 #确保shell 切换到当前shell 脚本文件夹
 exec_env_current_file_path=$(cd "$(dirname "$0")"; pwd)
 cd ${exec_env_current_file_path}
-
-pip3 download -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
+pip install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple/
+pip download -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
 EOF
 
 
